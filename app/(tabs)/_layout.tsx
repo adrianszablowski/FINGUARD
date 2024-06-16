591524de5fe3acf859c3e8c4d3cbb06aceb3495b
Adrian <adrian.szablowski.kontakt@gmail.com>
2024-06-16 13:28:22 +0200

    styled tabBar icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,42 @@
+import { TabBarIcon } from "@/components/navigation/TabBarIcon";
 import { Tabs } from "expo-router";
 import React from "react";
 
 const TabLayout = () => {
   return (
     <Tabs>
-      <Tabs.Screen name="home" options={{ headerShown: false }} />
-      <Tabs.Screen name="create" options={{ headerShown: false }} />
-      <Tabs.Screen name="profile" options={{ headerShown: false }} />
+      <Tabs.Screen
+        name="home"
+        options={{
+          headerShown: false,
+          tabBarLabel: "Home",
+          tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
+          tabBarInactiveTintColor: "#666",
+          tabBarActiveTintColor: "#000",
+        }}
+      />
+      <Tabs.Screen
+        name="create"
+        options={{
+          headerShown: false,
+          tabBarLabel: "Create",
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon name="add-circle" color={color} />
+          ),
+          tabBarInactiveTintColor: "#666",
+          tabBarActiveTintColor: "#000",
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          headerShown: false,
+          tabBarLabel: "Profile",
+          tabBarIcon: ({ color }) => <TabBarIcon name="person" color={color} />,
+          tabBarInactiveTintColor: "#666",
+          tabBarActiveTintColor: "#000",
+        }}
+      />
     </Tabs>
   );
 };
