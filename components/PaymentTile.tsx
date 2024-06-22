600b132147d9261987a241e2bff56b62f908847a
Adrian <adrian.szablowski.kontakt@gmail.com>
2024-06-22 15:20:07 +0200

    style paid button based on state

--- components/PaymentTile.tsx
+++ components/PaymentTile.tsx
@@ -1,8 +1,10 @@
 import { View, Text, TouchableOpacity } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "expo-router";
 
 const PaymentTile = () => {
+  const [paid, setPaid] = useState(false);
+
   return (
     <Link href="/edit" asChild>
       <TouchableOpacity className="mb-2 flex flex-row items-center justify-between rounded-md border border-gray-100 bg-white p-4">
@@ -11,9 +13,13 @@ const PaymentTile = () => {
           <Text>Due: 1.06.2024</Text>
           <Text>Recurring: monthly until 1.06.2025</Text>
         </View>
-        <View className="items-end gap-1">
+        <View className="items-end">
           <Text className="font-rbold">1200.00 PLN</Text>
-          <Text className="text-red-500">Unpaid</Text>
+          <TouchableOpacity className="p-2 pr-0" onPress={() => setPaid(!paid)}>
+            <Text className={`${paid ? "text-green-500" : "text-red-500"}`}>
+              {paid ? "Paid" : "Unpaid"}
+            </Text>
+          </TouchableOpacity>
         </View>
       </TouchableOpacity>
     </Link>
