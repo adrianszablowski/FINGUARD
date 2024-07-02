2a469964854b82066e01ea9204ab9836ea7d05d7
Adrian <adrian.szablowski.kontakt@gmail.com>
2024-07-02 17:22:55 +0200

    removed unused images and created landing page

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,26 @@
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Image, Text, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View className="flex flex-1 items-center justify-center font-rbold">
-      <Text className="text-2xl">FINGUARD</Text>
-      <Link href="/home" className="text-2xl">
-        Go to home
-      </Link>
-    </View>
+    <SafeAreaView className="flex flex-1 items-start justify-start gap-5 px-5 py-6 font-rbold">
+      <Text className="self-center font-rbold text-2xl">FINGUARD</Text>
+      <Image
+        source={require("../assets/images/shield.webp")}
+        className="h-80 w-80 self-center"
+      />
+      <Text className="font-rbold text-3xl">Take Control of Your Expenses</Text>
+      <Text className="text-gray-400">
+        Our mobile cost-tracking app helps you stay on top of your spending.
+        Easily log and categorize your expenses to gain valuable insights and
+        reach your financial goals.
+      </Text>
+      <TouchableOpacity className="rounded-md bg-black px-5 py-2">
+        <Link href="/home" className="text-2xl text-white">
+          Log In
+        </Link>
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 }
