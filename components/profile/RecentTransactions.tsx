383d70dbcf842913126909b2aa97191f9326fe6e
Adrian <adrian.szablowski.kontakt@gmail.com>
2024-07-08 23:19:57 +0200

    created basic recent transations card

--- components/profile/RecentTransactions.tsx
+++ components/profile/RecentTransactions.tsx
@@ -9,7 +9,7 @@ const RecentTransactions = () => {
       <FlatList
         data={[{}, {}, {}]}
         renderItem={(category) => <RecentTransation />}
-      ></FlatList>
+      />
     </View>
   );
 };
--- components/profile/RecentTransation.tsx
+++ components/profile/RecentTransation.tsx
@@ -1,10 +1,18 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { Ionicons } from "@expo/vector-icons";
 
 const RecentTransation = () => {
   return (
-    <View className="border border-gray-200">
-      <Text>RecentTransation</Text>
+    <View className="mb-4 flex-row items-center justify-between rounded-md border border-gray-200 p-5">
+      <View className="flex-row items-center gap-4">
+        <Ionicons name="bag-outline" size={24} color="gray" />
+        <View className="space-y-1">
+          <Text className="text-lg font-medium">Groceries</Text>
+          <Text className="text-gray-400">Oct 15, 2023</Text>
+        </View>
+      </View>
+      <Text className="text-lg font-medium">57.23 PLN</Text>
     </View>
   );
 };
